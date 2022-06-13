41748a7b6a2535708a4546aad361684dcaa00b9a
saehee15 <sae_hwi@naver.com>
2022-06-13 21:59:48 +0900

    20220613_날짜 클래스

--- chapter10/README.md
+++ chapter10/README.md
@@ -1,9 +1,12 @@
 # Chapter 10 날짜와 시간 & 형식화
 
 ## Calendar 클래스 
-: 추상 클래스 이므로 getInstance()를 통해 구현된 객체를 얻어야 한다.   
-: get()으로 날짜와 시간 필드 가져오기
+- 추상 클래스 이므로 getInstance()를 통해 구현된 객체를 얻어야 한다.   
+- get()으로 날짜와 시간 필드 가져오기
 ```java
 Calender cal = new Calendar(); // 에러!!! 추상클래스는 인스턴스 생성 불가
 Calender cal = Calendar.getInstance(); // OK, getInstance()메서드는 Calendar클래스를 구현한 클래스의 인스턴스를 반환한다
 ```
+- set()으로 날짜와 시간 지정하기
+- clear()는 Calendar객체의 모든 필드를 초기화
+- add()는 특정 필드의 값을 증가 또는 감소(다른 필드에 영향O)
---
+++ chapter10/deday.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+
+// date1과 date2 차이를 계산, 디데이 계산할때
+class deday {
+    public static void main(String[] args){
+        //  요일을 1부너 시작하기에, DAY_OF_WEEK[0]은 비어둠
+        final String[] DAY_OF_WEEK = { "", "월", "화", "수", "목", "금", "토", "일"};
+
+        Calendar date1 = Calendar.getInstance();
+        Calendar date2 = Calendar.getInstance();
+
+        date1.set( 2021, 9, 8);
+        System.out.println("date1은" + toString(date1) + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)] + "요일이고, ");
+        System.out.println("date2은" + toString(date2) + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)] + "요일이고, ");
+
+        // 두 날짜의 차이 -> 날짜를 초로 변환, 차이 구한 후 시간, 분, 초나 년, 월, 일로 변경
+        // getTimeInMillis() 천분의 일초 단위로 변환
+        long difference = 
+            (date2.getTimeInMillis() - date1.getTimeInMillis()) / 1000;
+        System.out.println("date1부터 date2까지는" + difference + "초가 지났습니다.");
+        System.out.println("일(day)로 계산하면" + difference/(24 * 60 * 60) + "일입니다."); //1일 = 24 * 60 * 60
+    }
+        public static String toString(Calendar date) {
+            return date.get(Calendar.YEAR) + "년" + (date.get(Calendar.MONTH)) + "월" 
+            + date.get(Calendar.DATE) + "일";
+        }
+
+}
